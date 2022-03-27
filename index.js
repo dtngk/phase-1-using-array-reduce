c99509055ec3cf9efd5711bee1da9b6287b27408
Dat Nguyen <dnguyenk2@gmail.com>
2022-03-27 13:37:54 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Code your solution herere
+const totalBatteries = batteryBatches.reduce((previous, current) => (previous + current)); 
